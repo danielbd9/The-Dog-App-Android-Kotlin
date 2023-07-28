00b0e4dd673c64fd7aacde479c1007b174ae7475
danie.diniz <danielbd9@hotmail.com>
2023-07-28 18:43:31 -0300

    Add Dagger Hilt Injection

---
+++ app/src/main/java/com/pt/SwordApplication.kt
@@ -0,0 +1,9 @@
+package com.pt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SwordApplication : Application() {
+
+}
--- app/src/main/java/com/pt/health/MainActivity.kt
+++ app/src/main/java/com/pt/health/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pt.health
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pt.sword.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
